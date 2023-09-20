0b5a59441a930426e403f18082484d9eaf74fac3
anisha_00 <anishastha0224@gmail.com>
2023-09-20 16:09:08 +0545

    adding js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,55 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+function validateLoginForm() {
+	var name = document.getElementById("logName").value;
+	var password = document.getElementById("logPassword").value;
+
+	if (name == "" || password == "") {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully logged in");
+		return true;
+	}
+}
+function validateSignupForm() {
+	var mail = document.getElementById("signEmail").value;
+	var name = document.getElementById("signName").value;
+	var password = document.getElementById("signPassword").value;
+    var Cpassword = document.getElementById("signConfPassword").value;
+    
+
+	if (mail == "" || name == "" || password == ""||Cpassword=="") {
+		document.getElementById("errorMsgs").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsgs").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+    else if(password.length == Cpassword.length){
+        document.getElementById("errorMsgs").innerHTML = "Your password is not matching"
+		return false;
+    }
+	else {
+		alert("Successfully signed up");
+		return true;
+	}
+}
